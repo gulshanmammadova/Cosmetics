5159aa8a6af86c2b871f262036efdf8457e5826a
gulshanmammadova <gulshanfm@code.edu.az>
2024-05-21 09:30:03 -0700

    contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header.jsx'
+import About from './pages/About/About';
+import Contact from './pages/Contact';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
 <Header/>
+<Routes>
+        {/* <Route path="/" element={<Home />}> */}
+          {/* <Route path="blogs" element={<Blogs />} /> */}
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+       
+          {/* <Route path="*" element={<NoPage />} /> */}
+      </Routes>
 <Footer/>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../Header/Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope,faLocationDot,faClock,faCartShopping } from '@fortawesome/free-solid-svg-icons';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Header = () => {
   return (
@@ -11,13 +13,32 @@ const Header = () => {
     <div className="header-top-content">
     <div className="w-92">
     <div className="header-top-content-left">
-<div>
-{/* <i class="fa-regular fa-clock white-i"></i> */}
-    {/* <FontAwesomeIcon icon="fa-regular fa-clock" /> */}
-    <span>Mon - Fri 8:00 - 18:00 / Sunday 8:00 - 14:00</span></div>
+<div >
+<FontAwesomeIcon icon={faEnvelope} className='right-10' />
+
+    <span>info@mail.com</span></div>
+    <div >
+<FontAwesomeIcon icon={faLocationDot} className='right-10' />
+
+    <span>234 Main Road , NY</span></div>
+    <div >
+<FontAwesomeIcon icon={faClock} className='right-10' />
+
+    <span>Mon- Fry 09 - 18</span></div>
 </div>
+
 <div className="header-top-content-right">
-<p className='header-login'>LOGIN</p>
+<div>
+  <span >
+  Term and Conditions
+  </span>
+  <span className='margin-12'>
+  TimeTable
+  </span>
+  <span>
+  Services
+  </span>
+</div>
 
 </div>
         </div>
@@ -25,7 +46,24 @@ const Header = () => {
 
     </div>
 <div className="header-content-bottom">
+<div className=' header-bottom-all  width-92'>
+  <div className="bottom-left">
+    <img className='logo' src="https://elementor-kits-02.nicdark.com/perfume-wordpress-elementor-kit/wp-content/uploads/sites/4/2024/01/logo.png" alt="" />
+  </div>
+  <div className="bottom-center">
+    <span>HOME</span>
+    <span>ABOUT</span>
+    <span>BLOG</span>
+    <span>SHOP</span>
+    <span>CONTACT</span>
 
+
+  </div>
+  <div className="bottom-right ">
+<span className='count-of-basket'>0</span>
+  <FontAwesomeIcon icon={faCartShopping} className='basket-icon'/>
+  </div>
+</div>
 </div>
 </div>
     </div>
